2db33e1b8b57dcceec952e628ef2f08d93ca39f6
Marcin Kalinowski <kalinowskixmarcin@o2.pl>
2024-04-29 12:49:35 +0200

    errorCard.tsx

---
+++ src/app/_components/errorCard.tsx
@@ -0,0 +1,29 @@
+import {Card, CardHeader, Divider} from "@nextui-org/react";
+import {type PropsWithChildren} from "react";
+
+interface IErrorCard {
+    desc: string;
+}
+
+const ExclamationCircleIcon = () =>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         fill="none" viewBox="0 0 24 24"
+         strokeWidth={1.5} stroke="currentColor"
+         className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round"
+              d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"/>
+    </svg>
+
+export default function ErrorCard({children, desc}: IErrorCard & PropsWithChildren) {
+
+    return (
+        <Card className="max-w-[400px] bg-red-600 text-white">
+            <CardHeader className="flex gap-3">
+                <ExclamationCircleIcon/>
+                <p className="text-sm">{desc}</p>
+                <div>{children}</div>
+            </CardHeader>
+            <Divider/>
+        </Card>
+    )
+};
